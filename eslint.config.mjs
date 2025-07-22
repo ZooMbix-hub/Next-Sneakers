b88613c26a51ba6a4391397d7c6a5aae9ea00a36
Your Name <you@example.com>
2025-07-22 23:49:29 +0500

    add Icon & variables color

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,22 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': 'warn',
+      'quotes': ['warn', 'single'],
+      'prefer-const': 'warn',
+      'indent': ['warn', 2],
+      'semi': ['warn', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      'react/prop-types': 0,
+      'no-trailing-spaces': 'error',
+      'no-useless-escape': 'warn'
+    }
+  }
 ];
 
 export default eslintConfig;
---
+++ src/assets/Icon/Icon.tsx
@@ -0,0 +1,13 @@
+import Favorite from './icons/favorite.svg';
+import Profile from './icons/profile.svg';
+import Check from './icons/check.svg';
+import Add from './icons/add.svg';
+
+export const Icon = {
+  Favorite,
+  Profile,
+  Check,
+  Add
+};
+
+export type IconType = keyof typeof Icon;
