fe7727ad20b38a1f1344e0c052db6e6917c70b80
Your Name <you@example.com>
2025-07-28 00:52:51 +0500

    feat: добавление активного таба

--- src/app/layouts/PublicLayout/ui/Navigation/Navigation.tsx
+++ src/app/layouts/PublicLayout/ui/Navigation/Navigation.tsx
@@ -1,20 +1,29 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import cn from 'classnames';
 import { Icon } from '@/src/assets';
 import { ROUTES } from '@/src/shared/routes';
 import { Typography } from '@/src/shared/ui/Typography';
 import s from './Navigation.module.css';
 
 export function Navigation() {
+  const pathname = usePathname();
+
   return (
     <nav>
       <ul className={s.linkContainer}>
         <li>
-          <Link className={s.link} href={ROUTES.basket}>
+          <Link className={cn(s.link, {[s.active]: pathname === ROUTES.basket})} href={ROUTES.basket}>
             <Icon.Basket />
+            <Typography weight={'bold'}>
+              {'0 руб.'}
+            </Typography>
           </Link>
         </li>
         <li>
-          <Link className={s.link} href={ROUTES.deferred}>
+          <Link className={cn(s.link, {[s.active]: pathname === ROUTES.deferred})} href={ROUTES.deferred}>
             <Icon.Defferred />
             <Typography color={'gray3'}>
               {'Закладки'}
@@ -22,7 +31,7 @@ export function Navigation() {
           </Link>
         </li>
         <li>
-          <Link className={s.link} href={ROUTES.profile}>
+          <Link className={cn(s.link, {[s.active]: pathname === ROUTES.profile})} href={ROUTES.profile}>
             <Icon.Profile />
             <Typography color={'gray3'}>
               {'Профиль'}
