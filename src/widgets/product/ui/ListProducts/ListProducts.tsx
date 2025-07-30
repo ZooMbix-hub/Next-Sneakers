09e861fd4a4fed1ebc24034a96aa9729cc90afd9
Your Name <you@example.com>
2025-07-31 00:05:35 +0500

    feat: создание компонента ListProducts

---
+++ src/widgets/product/ui/ListProducts/ListProducts.tsx
@@ -0,0 +1,9 @@
+import s from './ListProducts.module.css';
+
+export function ListProducts() {
+  return (
+    <div className={s.listProducts}>
+      ListProducts
+    </div>
+  );
+}
--- src/widgets/product/ui/index.ts
+++ src/widgets/product/ui/index.ts
@@ -1,1 +1,2 @@
 export { Card } from './Card/Card';
+export { ListProducts } from './ListProducts/ListProducts';
