5d4576e39e5c1fc9d758e8d0e443ec1c2d8fe650
Your Name <you@example.com>
2025-08-09 13:44:49 +0500

    feat: добавление redis

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,4 +1,17 @@
 import { Pool } from 'pg';
+import { createClient } from 'redis';
+
+const client = await createClient();
+// eslint-disable-next-line no-console
+client.on('error', err => console.log(err));
+
+if (!client.isOpen) {
+  client.connect();
+}
+
+export const add = (key: string, value: string) => client.set(key, value);
+
+export const get = (key: string) => client.get(key);
 
 export const db = new Pool({
   user: process.env.DB_USER,
