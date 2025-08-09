437e374cb93b513dcef1e3b7fa64ac347a987a96
Your Name <you@example.com>
2025-08-09 23:39:25 +0500

    refactor: удаление getFromRedis

--- src/widgets/product/ui/ListProducts/ListProducts.tsx
+++ src/widgets/product/ui/ListProducts/ListProducts.tsx
@@ -1,11 +1,9 @@
-import { getFromRedis } from '@/app/lib/db';
 import { getProducts } from '../../api';
 import { Card } from '../Card/Card';
 import s from './ListProducts.module.css';
 
 export async function ListProducts({ filter }: { filter: string }) {
   const products = await getProducts({ filter });
-  console.log(await getFromRedis('testKey'));
 
   return (
     <div className={s.listProducts}>
