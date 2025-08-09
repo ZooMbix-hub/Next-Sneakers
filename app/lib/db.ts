9356704cc806d86217285b7c983ecb2fbc466f4b
Your Name <you@example.com>
2025-08-09 17:59:32 +0500

    refactor: обновил конфиги для redis

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,17 +1,37 @@
 import { Pool } from 'pg';
 import { createClient } from 'redis';
 
-const client = await createClient();
-// eslint-disable-next-line no-console
-client.on('error', err => console.log(err));
+const client = createClient({
+  socket: {
+    reconnectStrategy: (retries) => {
+      if (retries < 10) {
+        return Math.min(retries * 50, 500);
+      } else {
+        return new Error('Max retries reached');
+      }
+    }
+  }
+});
 
-if (!client.isOpen) {
-  client.connect();
+// eslint-disable-next-line no-console
+client.on('error', (err) => console.log('Redis Client Error:', err));
+
+async function initializeRedis() {
+  if (!client.isOpen) {
+    await client.connect();
+  }
 }
 
-export const setToRedis = (key: string, value: string) => client.set(key, value);
+initializeRedis();
 
-export const getFromRedis = (key: string) => client.get(key);
+export const setToRedis = async (key: string, value: any) => {
+  await client.set(key, JSON.stringify(value));
+};
+
+export const getFromRedis =  async (key: string) => {
+  const value = await client.get(key);
+  return value;
+};
 
 export const db = new Pool({
   user: process.env.DB_USER,
