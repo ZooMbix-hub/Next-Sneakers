67865d704ba00585a2b4b59754e495a425bad321
Your Name <you@example.com>
2025-08-15 00:14:36 +0500

    feat: добавил новый метод в бд - получение кол-ва товаров

--- src/widgets/product/api/index.ts
+++ src/widgets/product/api/index.ts
@@ -1,4 +1,5 @@
 export { getProducts } from './requests/get-products';
 export { getProduct } from './requests/get-product';
+export { getCountProducts } from './requests/get-count-products';
 
 export type { Product } from './requests/types';
