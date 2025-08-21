d1b8f734d99d6b43c0c01d5a9e2c85383749fb36
Your Name <you@example.com>
2025-08-22 00:35:15 +0500

    feat: добавление контента в AuthorizationForm

--- src/features/auth/ui/Authorization/AuthorizationForm.tsx
+++ src/features/auth/ui/Authorization/AuthorizationForm.tsx
@@ -1,11 +1,25 @@
+'use client';
+
+import { useActionState } from 'react';
+import { Typography } from '@/src/shared/ui/Typography';
 import s from './Authorization.module.css';
 
-export function AuthorizationForm({}) {
+export function AuthorizationForm() {
+  /* const {} = useActionState(); */
+
   return (
-    <div className={s.authForm}>
-      <form>
-        AuthorizationForm
-      </form>
-    </div>
+    <form className={s.authForm}>
+      <div className={s.field}>
+        <Typography variant={'caption'}>
+          {'Email'}
+        </Typography>
+      </div>
+      <div className={s.field}>
+        <Typography variant={'caption'}>
+          {'Пароль'}
+        </Typography>
+      </div>
+      <button>{'Войти'}</button>
+    </form>
   );
 }
