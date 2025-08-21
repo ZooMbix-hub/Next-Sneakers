6cb7aee32fe7f77d21a6dc106c135f2128d4708f
Your Name <you@example.com>
2025-08-22 01:07:50 +0500

    feat: Добавление иконок Key, Mail

---
+++ src/assets/Icon/icons/Mail.tsx
@@ -0,0 +1,7 @@
+import type { IconProps } from '../Icon';
+
+export const Mail = (props: IconProps) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" color="#BDBDBD" viewBox="0 0 16 16" {...props}>
+    <path fill="currentColor" d="M1.18 6.423A7 7 0 1 1 14.808 9.62a2.499 2.499 0 0 1-4.403.92 3.5 3.5 0 1 1 1.005-1.753 1 1 0 0 0 1.949.452l.056-.277a5.5 5.5 0 1 0-2.938 3.949.75.75 0 0 1 .677 1.339A7 7 0 0 1 1.18 6.423m7.27-.371a2 2 0 1 0-.9 3.897 2 2 0 0 0 .9-3.897"/>
+  </svg>
+);
